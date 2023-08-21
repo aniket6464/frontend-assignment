af6cc0831f67c495f99727abdf1a3f82c13eac96
Aniket <aasamudre_b20@ce.vjti.ac.in>
2023-08-21 15:57:02 +0530

    pushing

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+            <nav>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact us</li>
+            </nav>
+            <div className="container">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p>
+            </div>
+    </>
+  );
+}
+
+export default App;
